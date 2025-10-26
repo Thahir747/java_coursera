79a89f6c4644db802d3b09c679ff1e73b5805a5d
Thahir <thahirhm6@gmail.com>
2025-10-26 17:30:34 +0530

    Updated on: 2025-10-26 17:30:34

---
+++ Manipulating.java
@@ -0,0 +1,51 @@
+/*public class Manipulating {
+    public static void main(String[] args) {
+
+    	String welcomeToJavaString = "Welcome java";
+    	// “welcomeToJavaStringInUpperCase” will be “WELCOME JAVA”
+    	String welcomeToJavaStringInUpperCase = welcomeToJavaString.toUpperCase();
+ 
+    	System.out.println("The string  " + welcomeToJavaString  + " in upper case is : " + welcomeToJavaStringInUpperCase );
+	
+    }
+    
+}
+
+
+public class Manipulating {
+	public static void main(String[] args) {
+    	// “welcomeUseDirectString” is now "Welcome java"
+    	String welcomeUseDirectString = "Welcome java".toUpperCase();
+ 
+    	System.out.println("The string Welcome java in upper case is : " + welcomeUseDirectString );
+ 
+	}
+}
+
+public class Manipulating {
+	public static void main(String[] args) {
+    	String testString = "This is a test";
+ 
+    	boolean result1 = testString.startsWith("test"); // false
+    	boolean result2 = testString.startsWith("This"); // true
+ 
+        System.out.println("testString.startsWith(\"test\") : " + testString.startsWith("test"));
+        System.out.println("testString.startsWith(\"This\") : " + testString.startsWith("This"));
+ 
+	}
+}
+
+public class Manipulating {
+	public static void main(String[] args) {
+    	String testString = "This is a test";
+ 
+    	boolean result1 = testString.startsWith("test"); // false
+    	boolean result2 = testString.startsWith("This"); // true
+ 
+        System.out.println("testString.startsWith(\"test\") : " + result1);
+        System.out.println("testString.startsWith(\"This\") : " + result2);
+ 
+	}
+}*/
+
+
