5709d0642ec96eccd7951c18ce3114eb4168b06c
Thahir <thahirhm6@gmail.com>
2025-11-04 23:59:26 +0530

    Updated on: 2025-11-04 23:59:25

---
+++ classy-shapes-project/src/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle {
+    double length;
+    double width;
+
+    public Rectangle() {
+        this.length = 5.0;
+        this.width = 3.0;
+    }
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+}
