265b14386a92d0fef4982df5252884e55534bd5f
Thahir <thahirhm6@gmail.com>
2025-12-17 19:18:42 +0530

    Updated on: 2025-12-17 19:18:42

---
+++ method1.java
@@ -0,0 +1,11 @@
+public class method1 {
+
+    public static void main(String[] args){
+
+        public static void setSquare(){
+            
+        }
+
+    }
+    
+}
---
+++ rewind methods/untitled/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    // Area of a square = side * side
+    static double sideOfSquare;
+
+    // method to set the side of the square
+    public static void setSquareSide() {
+        sideOfSquare = 5.2;
+    }
+
+    // method to calculate the area of the square
+    public static double calculateAreaOfSquare() {
+        double areaOfSquare =sideOfSquare * sideOfSquare;
+        return  areaOfSquare;
+    }
+
+    // the main method
+    public static void main(String[] args) {
+// set the side of square
+        setSquareSide();
+
+// call the method to calculate the area of the square
+// And store the result given back by the method in a variable
+        calculateAreaOfSquare();
+
+// print the result, using the values from the variables
+        System.out.println("Area of the square with side " + sideOfSquare + " is: " + calculateAreaOfSquare());
+    }
+}
