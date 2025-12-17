4bcee0c949a11509e437719d9d870aa1729f4521
Thahir <thahirhm6@gmail.com>
2025-12-17 19:23:05 +0530

    Updated on: 2025-12-17 19:23:04

--- auto1.sh
+++ auto1.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-git init
+if [ ! -d ".git" ]; then
+  git init
+  git remote add origin https://github.com/Thahir747/java_coursera
+fi
+
 git add .
 git commit -m "Updated on: $(date '+%Y-%m-%d %H:%M:%S')"
-git remote add origin https://github.com/Thahir747/java_coursera
-git push -u origin master
+git pull --rebase origin master
+# Push changes
+git push origin master
